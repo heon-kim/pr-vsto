d26ccb87b7c61c9ae86eeffa48466c7574f42165
heon-kim <vndtka123@gmail.com>
2022-07-08 06:38:45 +0900

    pr

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Homepage title="SHEILDInfo" />
+    <Homepage msg="등급이 지정되지 않았습니다." />
+    <img alt="mainImg" src="./assets/logo.png" />
+    <img alt="SHEILDInfo logo" src="./assets/logo.png" />
+  </div>
+</template>
+
+<script>
+import Homepage from "./components/Homepage.vue";
+// import selectLabel from "./components/selectLabel.vue";
+// import selectGrade from "./components/seletGrade.vue";
+
+export default {
+  name: "App",
+  components: {
+    Homepage,
+    // selectLabel,
+    // selectGrade,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Homepage.vue
@@ -0,0 +1,30 @@
+f
+<template>
+  <div>
+    <div class="title">
+      {{ title }}
+    </div>
+    <div class="main">
+      <p>{{ msg }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  props: {
+    title: String,
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.title {
+  background-color: #0167b8;
+  color: white;
+  text-align: left;
+}
+</style>
